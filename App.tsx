c463c7a826c98c94a14ffe8ab215d34a89ab54b6
Vaibhav Thapliyal <54322183+vaibhavthapliyalx@users.noreply.github.com>
2023-03-11 12:59:33 +0000

    Splash screen added (#6)
    Co-authored-by: Vaibhav Thapliyal <vaibhavthapliyal@Vaibhavs-MBP.broadband>

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 //--------------- Copyright (c) 2023 WattPay. ---------------//
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import GenerateInvoiceScreen from './android/components/GenerateInvoiceScreen';
 import LoginPage from './android/components/LoginPage';
 
+
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <GenerateInvoiceScreen/>
